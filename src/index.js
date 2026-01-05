468304445730d3ab4710052775b7e08098211427
Sheila Kolenbrander <Sheila.Kolenbrander@etesian.nl>
2026-01-05 15:48:18 +0100

    chore: bump version tot 1.1.0

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({
         message: 'Welcome to the Les 6 API!',
-        version: process.env.APP_VERSION || '1.0.0',
+        version: process.env.APP_VERSION || '1.1.0',
         environment: process.env.NODE_ENV || 'development'
     });
 });
